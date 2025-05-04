a9648f19e1249d58342ea1a11580f2c9a7422226
Hannah <hannah.gabrielac@gmail.com>
2025-05-03 22:22:30 -0300

    adicionando função de remover card

--- frontend/js/produtos.js
+++ frontend/js/produtos.js
@@ -4,13 +4,26 @@ const container = document.getElementById("produtos-container");
 
 produtos.forEach(function(produto) {
   const cardProd = `
-    <div class="card">
+    <div class="card" id="produto-${produto.nome}">
       <img src="${produto.imagem}" class="card-img" alt="${produto.nome}">
       <div class="card-body">
+        <span class="fa-solid fa-trash delete-icon"></span>
         <h5 class="card-title">${produto.nome}</h5>
         <p class="card-title">${produto.tipo}</p>
       </div>
     </div>
   `;
+
   container.insertAdjacentHTML("beforeend", cardProd);
+
+  const trashIcon = document.querySelector(
+    `#produto-${produto.nome} .delete-icon`
+  );
+
+  trashIcon.onclick = function () {
+    const cardToRemove = document.querySelector(`#produto-${produto.nome}`);
+    cardToRemove.remove();
+  };
 });
+
+
