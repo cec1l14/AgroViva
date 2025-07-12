bb193fbd856b318b8e33004c228d38ba892d0a7e
isa-lpq <isabella.santana@academico.ifpb.edu.br>
2025-07-12 23:21:32 +0000

    relacionamento entre produto e produtor

--- backend/src/database/migration.js
+++ backend/src/database/migration.js
@@ -3,26 +3,31 @@ import Database from './database.js';
 async function up() {
   const db = await Database.connect();
 
+  const produtorSql = `
+    CREATE TABLE IF NOT EXISTS produtor (
+      cod_produtor INTEGER PRIMARY KEY AUTOINCREMENT,
+      email VARCHAR(40) NOT NULL,
+      telefone NUMERIC(13) NOT NULL,
+      cod_id INTEGER NOT NULL,
+      senha VARCHAR(40) NOT NULL,
+      cpf NUMERIC(11) NOT NULL
+    );
+  `;
+
   const produtosSql = `
     CREATE TABLE IF NOT EXISTS produtos (
       cod_produto INTEGER PRIMARY KEY AUTOINCREMENT,
       validade DATE,
       preco FLOAT,
-      descricao VARCHAR(40)
+      descricao VARCHAR(40),
+      cod_produtor INTEGER NOT NULL,
+      FOREIGN KEY (cod_produtor) REFERENCES produtor(cod_produtor)
     );
   `;
-  const produtorSql = `
-    CREATE TABLE if note exists produtor (
-      cod_produtor integer primary key autoincremet,
-      email varchar (40) not null,
-      telefone numeric (13) not null,
-      cod_id integer not null,
-      senha varchar (40) not null,
-      cpf numeric (11) not null
-    );
-  `;
+
+  await db.run(produtorSql);
   await db.run(produtosSql);
-  await db.run (produtorSql);
+
 }
 
-export default { up };
+up();
